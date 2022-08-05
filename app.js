e0b2e90f80f059e540da148b0534b628c589c0ed
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-05 21:23:33 +0530

    by given the location in geocoding it return long and lat so this given to weater api we can get the weather

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const axios=require('axios')
 
-axios.get('http://api.weatherstack.com/current?access_key=37df30cafde4731f312e687232100897&query=28.644800,77.216721')
+const axios = require('axios')
+axios.get('https://geocode.maps.co/search?q=toronto')
+.then((res)=>{
+    // console.log(res.data[0].lat,res.data[0].lon);
+axios.get(`http://api.weatherstack.com/current?access_key=37df30cafde4731f312e687232100897&query=${res.data[0].lat},${res.data[0].lon}`)
 .then((res)=>{
     // console.log(res.data.current.temperature,res.data.current.feelslike);
     console.log(`It is currently ${res.data.current.temperature} degress out. It feels like ${res.data.current.feelslike} degress out`);
 })
-
-
-
+})
