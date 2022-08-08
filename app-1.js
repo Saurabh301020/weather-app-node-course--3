7c9fde906429d91eadb7675e5a1d764de8770b36
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-08 17:30:42 +0530

    setup the handlebars for showing the ui to user

--- app-1.js
+++ app-1.js
@@ -1,7 +1,35 @@
+const path=require('path')
 const weatherFunc=require('./utils/weather-api');
 const geoCoding=require('./utils/geocoding-api');
 const express=require('express')
 const app=express()
+const hbs=require('hbs')
+
+const viewPaths=path.join(__dirname,'/templates/views')
+const partialsPath=path.join(__dirname,'/templates/partials')
+const staticPath=path.join(__dirname,'/public')
+
+app.use(express.static(staticPath))
+
+app.set('view engine','hbs')
+app.set('views',viewPaths)
+hbs.registerPartials(partialsPath)
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Saurabh'
+    })
+})
+app.get('',(req,res)=>{
+    res.send('this is from server!')
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Saurabh'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     // console.log(req.query); 
